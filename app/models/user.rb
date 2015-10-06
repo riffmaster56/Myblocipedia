de068294ca93ec913d9dedadb553a650f1504563
Alex <riffmaster56@gmail.com>
2015-10-05 18:32:17 -0700

    added role to users signing up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  after_initialize :set_default_role
+
+  def set_default_role
+    self.role ||= :standard
+  end
 
   def admin?
     role == 'admin'
